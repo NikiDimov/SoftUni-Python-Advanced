310ac9d6cdca54218cdf3e1ab0f765678bde949c
niki <niki.dimov86@gmail.com>
2021-02-09 22:35:06 +0200

    new

---
+++ Functions Advanced/age_assignment.py
@@ -0,0 +1,12 @@
+def age_assignment(*args, **kwargs):
+    result = {}
+    for name in args:
+        for key, value in kwargs.items():
+            if key == name[0]:
+                result[name] = value
+
+    return result
+
+
+print(age_assignment("Peter", "George", G=26, P=19))
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
