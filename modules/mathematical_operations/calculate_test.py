5536c9e974f911f8a7354be0d1caff15847dcd58
niki <niki.dimov86@gmail.com>
2021-02-19 21:15:20 +0200

    new

---
+++ modules/mathematical_operations/calculate_test.py
@@ -0,0 +1,5 @@
+from advanced.modules.mathematical_operations.calculate_expression import calculate_expression
+
+print(calculate_expression("2.5 * 2"))
+print(calculate_expression("6.66 ^ 2"))
+print(calculate_expression("36.66 / 6"))
