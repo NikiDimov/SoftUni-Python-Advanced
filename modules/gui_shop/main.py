7974ca9599cafcb451b9f01f71743303adc82a39
niki <niki.dimov86@gmail.com>
2021-02-27 17:48:46 +0200

    by Ines

---
+++ modules/gui_shop/main.py
@@ -0,0 +1,9 @@
+from advanced.modules.gui_shop.canvas import tk
+from advanced.modules.gui_shop.authentication import render_main_enter_screen
+
+
+if __name__ == "__main__":
+    render_main_enter_screen()
+
+    tk.mainloop()
+
